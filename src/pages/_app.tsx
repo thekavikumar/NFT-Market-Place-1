38e03a8e2fbd189490e9fd34a19e650c9107d8d8
thekavikumar <kavikumarceo@gmail.com>
2023-03-29 23:01:24 +0530

    nftDropPage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { EthereumClassicTestnetMorden } from "@thirdweb-dev/chains";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider activeChain={EthereumClassicTestnetMorden}>
+      <Component {...pageProps} />{" "}
+    </ThirdwebProvider>
+  );
 }
